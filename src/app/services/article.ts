9d73f4b6bf68e54334cda8b9b0c720593a5b376d
AliIdris1 <166216558+AliIdris1@users.noreply.github.com>
2024-09-21 17:03:03 +0300

    Update article.ts

--- src/app/services/article.ts
+++ src/app/services/article.ts
@@ -10,7 +10,7 @@ export const articleApi = createApi({
     baseQuery: fetchBaseQuery({
         baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com/',
         prepareHeaders: (headers:any) => {
-            headers.set('x-rapidapi-key' , rapidApiKey);
+            headers.set('x-rapidapi-key' , 'f3d261b412msh1f57a16a193f235p1c7eaejsnd4eb1342e487');
             headers.set('x-rapidapi-host' , 'article-extractor-and-summarizer.p.rapidapi.com')
 
             return headers
