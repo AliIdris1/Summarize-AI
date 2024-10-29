225a7bc05e457ef5a661a8b8e8c3876a420239a6
AliIdris1 <166216558+AliIdris1@users.noreply.github.com>
2024-10-29 22:29:16 +0300

    Update article.ts

--- src/app/services/article.ts
+++ src/app/services/article.ts
@@ -2,7 +2,6 @@ import { createApi , fetchBaseQuery  } from "@reduxjs/toolkit/query/react";
 import { env } from "process";
 
 const rapidApiKey = process.env.NEXT_RAPID_API_ARTICLE_KEY
-console.log(rapidApiKey)
 
 
 export const articleApi = createApi({
@@ -10,7 +9,7 @@ export const articleApi = createApi({
     baseQuery: fetchBaseQuery({
         baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com/',
         prepareHeaders: (headers:any) => {
-            headers.set('x-rapidapi-key' , 'f3d261b412msh1f57a16a193f235p1c7eaejsnd4eb1342e487');
+            headers.set('x-rapidapi-key' , rapidApiKey);
             headers.set('x-rapidapi-host' , 'article-extractor-and-summarizer.p.rapidapi.com')
 
             return headers
